2319abf6426f43750b3ff27ccc06ce1062b83a2c
Nawed Ali <nawed2611@gmail.com>
2022-10-01 18:55:11 +0530

    add prettier formatting

--- cohere.js
+++ cohere.js
@@ -1,19 +1,19 @@
-const env = require(`dotenv`).config();
-const cohere = require('cohere-ai');
+const env = require(`dotenv`).config()
+const cohere = require('cohere-ai')
 
-cohere.init(process.env.API_KEY);
-(async () => {
-    const response = await cohere.generate({
-        model: 'xlarge',
-        prompt: 'What are sharks?',
-        max_tokens: 50,
-        temperature: 0.9,
-        k: 0,
-        p: 0.75,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-        stop_sequences: [],
-        return_likelihoods: 'NONE'
-    });
-    console.log(`Prediction: ${response.body.generations[0].text}`);
-})();
+cohere.init(process.env.API_KEY)
+    (async () => {
+        const response = await cohere.generate({
+            model: 'xlarge',
+            prompt: 'What are sharks?',
+            max_tokens: 50,
+            temperature: 0.9,
+            k: 0,
+            p: 0.75,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+            stop_sequences: [],
+            return_likelihoods: 'NONE'
+        })
+        console.log(`Prediction: ${response.body.generations[0].text}`)
+    })()
